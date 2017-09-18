bff661bb76dd251e1423cd74acabf4d290260b7b
brcla <brcla@DESKTOP-RLIRDNN>
2017-09-18 13:19:10 -0500

    Added documentation and cleaned code

--- src/edu/txstate/cs3320/claggett/PlayWriter.java
+++ src/edu/txstate/cs3320/claggett/PlayWriter.java
@@ -17,12 +17,22 @@ public class PlayWriter {
 	private static String output = "./iofiles/MidsummerNightsDream.txt";
 	private static BufferedWriter  writer = null;
 	
+	/**
+	 * The main method to start the reading and writing of the play
+	 * @param args
+	 */
 	public static void main(String []args){
 		String thePlay = PlayJSONReader.readPlay(input);
 		writePlay(thePlay, output);
 		
 	}
-	
+	/**
+	 * responsible for handling the writing of the play by calling helper methods 
+	 * that use a "top-down" approach. Takes the the Play as a string, writes the output 
+	 * to a text file
+	 * @param thePlay
+	 * @param outputFile
+	 */
 	public static void writePlay(String thePlay, String outputFile) {
 		
 		
@@ -32,16 +42,21 @@ public class PlayWriter {
 			e.printStackTrace();
 		}
 		
-		
+		//takes the string of the play and parses it as a jsonObject
 		JsonElement jelement = new JsonParser().parse(thePlay);
 		JsonObject thePlayJsonObject = (JsonObject)jelement;
+		
 		writePlayTitleAndYear(thePlayJsonObject);
 		writeActs(thePlayJsonObject);
 		
 		
 		
 	}
-	
+	/**
+	 * Takes the jsonObject of the entire play and writes the title and year to the 
+	 * output file
+	 * @param thePlayJson
+	 */
 	private static void writePlayTitleAndYear(JsonObject thePlayJson) {
 		
 		try {
@@ -62,31 +77,49 @@ public class PlayWriter {
 		
 		
 	}
-	
+	/**
+	 * Takes the play as a jsonObject and extracts the acts as plain text
+	 * Then it iterates through each act calls the helper 
+	 * method writeAct to write the text in the act
+	 * @param thePlayJson
+	 */
 	private static void writeActs(JsonObject thePlayJson) {
 		
 		ArrayList<String> acts = AccessorUtils.getActsOfPlayByName(thePlayJson);
 		for (int i = 0; i < acts.size(); i++) {
-			try {
-				writer.write((acts.get(i)) + "\n");
-				writer.flush();
-			} catch (IOException e) {
-				// TODO Auto-generated catch block
-				e.printStackTrace();
-			}
+			
+			//passes the act as a jsonObject to the writeAct function, also passes the name of the act for writing
 			writeAct(AccessorUtils.getActOfPlay(thePlayJson, acts.get(i)), acts.get(i));
 		}
 		
 	}
 	
-	
+	/**
+	 * Takes the actNameString and prints it at the beginning of each act, then
+	 * passes the actName JsonObject to the writeScenesOfAct helper function to 
+	 * write each scene of the act
+	 * @param actName
+	 * @param actNameString
+	 */
 	private static void writeAct(JsonObject actName, String actNameString) {
 		
+		try {
+			writer.write((actNameString) + "\n");
+			writer.flush();
+		} catch (IOException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}
 		writeScenesOfAct(actName, actNameString);
 
 	}
 	
-	
+	/**
+	 * Takes the actName as a JsonObject and iterates through each scene to print the header
+	 * for each scene and passes the scene JsonObject to the writeSceneOfAct helper function
+	 * @param actName
+	 * @param actNameString
+	 */
 	private static void writeScenesOfAct(JsonObject actName, String actNameString) {
 		
 		ArrayList<String> scenes = AccessorUtils.getScenesOfActByName(actName);
@@ -105,7 +138,11 @@ public class PlayWriter {
 		
 	}
 	
-	
+	/**
+	 * takes the JsonObject of the scene and iterates through each paragraph to call the 
+	 * writeSceneParagraphs helper function to write the text of each paragraph
+	 * @param scene
+	 */
 	private static void writeSceneOfAct(JsonObject scene) {
 		
 		JsonArray paragraphs = AccessorUtils.getSceneParagraphs(scene);
@@ -115,7 +152,10 @@ public class PlayWriter {
 		}
 	}
 
-	
+	/**
+	 * Takes the paragraph as a JsonObject and writes the text from each paragraph to the output file
+	 * @param paragraph
+	 */
 	private static void writeSceneParagraphs(JsonObject paragraph) {
 		try {
 			writer.write(AccessorUtils.getCharacterFromParagraph(paragraph) + "\n");
